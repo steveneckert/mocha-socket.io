c4131cd6857822d05345744a5b8f92335a11f5c8
Steven Eckert <stevene@quadrus.com>
2014-08-15 10:04:10 -0600

    moved to a lib folder

--- test/contextTests.js
+++ test/contextTests.js
@@ -2,22 +2,22 @@
 
 var chai = require('chai');
 var should = chai.should();
-var socketContext = require('../src/index');
+var socketContext = require('../lib/index');
 
-describe('should inherit a socket context', socketContext(function () {
+describe('should inherit a socket context', socketContext(function() {
 
-    it('should be true', function () {
+    it('should be true', function() {
         console.log('test1');
         true.should.be.true;
     });
 
-    describe('inner test context', function () {
+    describe('inner test context', function() {
 
-        it('should be true', function (done) {
+        it('should be true', function(done) {
             console.log('test2');
 
             var client = this.attach(true);
-            client.on('connect', function () {
+            client.on('connect', function() {
                 true.should.be.true;
                 done();
             });
